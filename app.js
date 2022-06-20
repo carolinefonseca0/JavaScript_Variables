9d14dec3b437a9abf8d2fc9bc8d1e5c657979a90
Caroline <103154901+carolinefonseca0@users.noreply.github.com>
2022-06-20 18:13:02 -0400

    second pass at exercise

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// Exercise 1
 const firstName = "Caroline";
 let lastName = "Fonseca";
 var age = 34;
+
+// Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
 let city = "Port Royal, SC";
 let pastime = "dancing";
-let fullName = "firstName lastName"
 
 console.log(fullName)
 
@@ -19,11 +24,11 @@ let inStock = false;
 let selectedSize = "M"
 
 let title = "Name of the Wind";
-let author = "Patrick Rothfuss"
+let author = "Patrick Rothfuss";
 let pageCount = 722;
 let bookmark = 456;
 let hasRead = true;
 
-let myStory = 'Hello, my name is ${fullName}. I live in ${city}. I enjoy ${pastime} and learning to code!'
+let myStory = `\ Hello, my name is ${fullName}. I live in ${city}. I enjoy ${pastime} and learning to code!`;
 
-console.log(myStory);
+console.log(myStory)
