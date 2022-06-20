dee2809e57175e559939895e89907465ade9c7e9
Caroline <103154901+carolinefonseca0@users.noreply.github.com>
2022-06-19 23:21:53 -0400

    attempted Java Intro exercise, no idea what the heck went wrong

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+const firstName = "Caroline";
+let lastName = "Fonseca";
+var age = 34;
+let city = "Port Royal, SC";
+let pastime = "dancing";
+let fullName = "firstName lastName"
 
-// Exercise 1
+console.log(fullName)
 
-// Exercise 2
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss"
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+let myStory = 'Hello, my name is ${fullName}. I live in ${city}. I enjoy ${pastime} and learning to code!'
+
+console.log(myStory);
