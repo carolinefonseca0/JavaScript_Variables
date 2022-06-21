185c70148892deae8ad4b770b744e69e1ad7646e
Caroline <103154901+carolinefonseca0@users.noreply.github.com>
2022-06-20 20:17:21 -0400

    Completed Operator exercises

---
+++ OperatorExercise.js
@@ -0,0 +1,46 @@
+// Exercise 1
+var a = 20;
+var b = 4;
+let add = a + b;
+console.log(add);
+
+let minus = a - b;
+console.log(minus);
+
+let multiply = a * b;
+console.log(multiply);
+
+let dividing = a / b;
+console.log(divide);
+
+// Exercise 2
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(c + str);
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(c + lastName);
+
+// Exercise 3
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3);
+console.log(d === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && d >= str3);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
